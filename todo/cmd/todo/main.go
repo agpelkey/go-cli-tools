78609fada79bf9f89fa52b54d69c2d027857a6d9
alex <agpelkey@yahoo.com>
2023-01-07 23:37:04 -0800

    first commit

---
+++ todo/cmd/todo/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"flag"
+
+	"github.com/agpelkey/todo"
+)
+
+// Hardcoding the file name
+const todoFileName = ".todo.json"
+
+func main(){
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+	"%s tool. Developed for learning purposes\n", os.Args[0])
+	fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2023\n")
+	fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+	flag.PrintDefaults()
+	}
+	
+
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	// Define an items list
+	l := &todo.List{}
+
+	// Use the Get method to read to do items from file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on the number of arguments provided
+	switch {
+	case *list:
+		// List current to do items
+		fmt.Print(l)
+		
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid Option")
+		os.Exit(1)
+	}
+}
